65ea6ddb74ced7aec9292f0843eaa4e6dd41f125
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-22 11:13:11 +0300

    test

--- database/task_6/combineUsers_migration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$sql = 'CREATE TABLE combineUsers (id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-firstname VARCHAR(20) NOT NULL,lastname VARCHAR(20) NOT NULL,email VARCHAR(40) NOT NULL UNIQUE, 
-password VARCHAR(60) NOT NULL, created_date TIMESTAMP)';
-
-return $sql;
